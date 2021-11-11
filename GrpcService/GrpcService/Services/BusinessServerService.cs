5b9f49793a35a761148f47b160b09f2ac7654ffe
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-11-11 09:27:35 +0100

    /Dorin

---
+++ GrpcService/GrpcService/Services/BusinessServerService.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService
+{
+    public class BusinessServerService : BusinessServer.BusinessServerBase
+    {
+        private string uri = "http://localhost:8080";
+        private readonly HttpClient client;
+        private readonly ILogger<BusinessServerService> _logger;
+
+        public BusinessServerService(ILogger<BusinessServerService> logger)
+        {
+            client = new HttpClient();
+            _logger = logger;
+        }
+
+        public override async Task<GroupReply> GetGroup(GroupRequest request, ServerCallContext context)
+        {
+            Console.WriteLine(request);
+            Task<string> stringAsync = client.GetStringAsync(uri + "/Group/" + request.Name);
+            string message = await stringAsync;
+            return await Task.FromResult(new GroupReply
+            {
+                Message = message
+            });
+        }
+
+        public override async Task<GroupReply> PostGroup(GroupRequest request, ServerCallContext context)
+        {
+            HttpContent content = new StringContent(request.Name, Encoding.UTF8, "application/json");
+            Console.WriteLine(2);
+            HttpResponseMessage responseMessage = await client.PutAsync(uri + "/Group", content);
+            Console.WriteLine(responseMessage.Content);
+
+            Task<string> stringAsync = client.GetStringAsync(uri + "/Group/" + "5");
+            string message = await stringAsync;
+            Console.WriteLine(message);
+            return await Task.FromResult(new GroupReply
+            {
+                Message = message
+            });
+        }
+    }
+}
