57c9fdbe0e6df9b1218f86fd9c2bb33f78fa602c
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-11-11 15:29:29 +0100

    /Dorin

---
+++ Tier1/PresentationLayer/Sep3Blazor/Data/IGroupService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sep3Blazor.Data
+{
+    public interface IGroupService
+    {
+        
+        public Task<IList<string>> GetGroups(String s);
+        
+        public Task<IList<string>> AddGroup(String s);
+    }
+}
