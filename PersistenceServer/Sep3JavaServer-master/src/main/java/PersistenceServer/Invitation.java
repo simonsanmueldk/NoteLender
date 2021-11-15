750e6746761c1237dbf3640e86bb56cb37197b44
simonsanmeldk <simonsamuelhol@gmail.com>
2021-11-15 13:05:39 +0100

    Invitation

---
+++ PersistenceServer/Sep3JavaServer-master/src/main/java/PersistenceServer/Invitation.java
@@ -0,0 +1,18 @@
+package PersistenceServer;
+
+public class Invitation
+{
+  private int id;
+
+  public int getId(int id)
+  {
+    return id;
+  }
+
+  public void setId(int id)
+  {
+    this.id = id;
+  }
+
+
+}
