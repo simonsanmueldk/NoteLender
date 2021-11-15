bac1f7d57ccf41e2031de04777fd4d38712913a4
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-11-15 13:09:49 +0100

    //Dorin 11/15/2021

--- PresentationLayer/Sep3Blazor-Soap/Sep3Blazor/Model/Note.cs
+++ PresentationLayer/Sep3Blazor-Soap/Sep3Blazor/Model/Note.cs
@@ -8,9 +8,9 @@ public class Note
 
     public int id{ get; set; }
 
-    public int user_id { get; set; };
+    public int user_id { get; set; }
 
-    public int group_id { get; set; };
+    public int group_id { get; set; }
     
     public int week{ get; set; }
  
