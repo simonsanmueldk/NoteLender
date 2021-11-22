f79b0ac6f02e3e8a99fc92391cc290fa407ec177
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-11-22 09:21:05 +0100

    //Pascal

---
+++ BusinessLogic/GrpcServiceSep3-master/GrpcService/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GrpcService.Model
+{
+    public class User
+    {
+        public int id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+
+        public User(int id, String firstName, String lastName, String username, String password)
+        {
+            this.id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Username = username;
+            Password = password;
+        }
+    }
+}
