aabbd5b438e75c19adca3add79f47f044d383a42
Hansatang <portokarlos@gmail.com>
2021-11-22 09:42:07 +0100

    22.11.2021 GRPC

---
+++ BusinessLogic/GrpcServiceSep3-master/GrpcService/Model/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sep3Blazor.Model
+{
+    public class Group
+    {
+        public int id{ get; set; }
+        public String name{ get; set; }
+
+        public Group(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
---
+++ BusinessLogic/GrpcServiceSep3-master/GrpcService/Model/Invitation.cs
@@ -0,0 +1,12 @@
+namespace Sep3Blazor.Model
+{
+    public class Invitation
+    {
+        public int id{ get; set; }
+
+        public Invitation(int id)
+        {
+            this.id = id;
+        }
+    }
+}
