e6915a826952633399fbc4d8afa611ad064ae5f4
Fogh1206 <esbenvensel.fogh@gmail.com>
2021-11-25 13:08:33 +0100

    777

--- PresentationLayer/Sep3Blazor-Soap/Sep3Blazor/Model/User.cs
+++ PresentationLayer/Sep3Blazor-Soap/Sep3Blazor/Model/User.cs
@@ -6,9 +6,12 @@ namespace Sep3Blazor.Model
     public class User
     {
         public int id { get; set; }
-        [Required] [StringLength(20)] public String FirstName { get; set; }
-        [Required] [StringLength(20)] public String LastName { get; set; }
-        [Required] [StringLength(15)] public String Username { get; set; }
+        [Required] [StringLength(20)] 
+        public String FirstName { get; set; }
+        [Required] [StringLength(20)] 
+        public String LastName { get; set; }
+        [Required] [StringLength(15)] 
+        public String Username { get; set; }
 
         [Required] /*[Range(7,15,ErrorMessage = "Please enter a value between 7-15 characters for Password")]*/
         public String Password { get; set; }
