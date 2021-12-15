4052bc1f26b598f8620c87204b3911b128911e5a
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-12-15 11:27:22 +0100

    /Dorin

---
+++ PresentationLayer/Sep3Blazor-Soap/TestProjectUserService/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using Sep3B
+
+namespace TestProjectUserService
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+            
+            Assert.True(true);
+        }
+    }
+}
