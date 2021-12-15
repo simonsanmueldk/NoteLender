19aa4e7b5f6f6d6fbf361a5249e732aa72971ff3
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-12-15 11:12:32 +0100

    /Dorin

---
+++ BusinessLogic/GrpcServiceSep3-master/GrpcService/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace GrpcService
+{
+    public class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+        
+            CreateHostBuilder(args).Build().Run();
+        }
+        
+        /// <summary>
+        /// Static method creates a Generic Host
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
---
+++ BusinessLogic/GrpcServiceSep3-master/GrpcService/Services/BusinessServerService.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcService.Logic;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService.Services
+{
+    public class BusinessServerService : BusinessServer.BusinessServerBase
+    {
+        /// <summary>
+        /// Instance variables
+        /// </summary>
+        private readonly ILogicService _logicService;
+        private readonly ILogger<BusinessServerService> _logger;
+
+        /// <summary>
+        /// 1-argument constructor
+        /// </summary>
+        /// <param name="logger"></param>
+        public BusinessServerService(ILogger<BusinessServerService> logger)
+        {
+            _logicService = new LogicService();
+            _logger = logger;
+        }
+        
+        /// <summary>
+        /// Method PostNote invokes the method with same name
+        /// with the retrieved parameters from NoteService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> PostNote(NoteRequest request, ServerCallContext context)
+        {
+            return await _logicService.PostNote(request, context);
+        }
+
+        /// <summary>
+        /// Method PutNote invokes the method with same name
+        /// with the retrieved parameters from NoteService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> PutNote(NoteRequest request, ServerCallContext context)
+        {
+            return await _logicService.PutNote(request, context);
+        }
+
+        /// <summary>
+        /// Method DeleteNote invokes the method with same name
+        /// with the retrieved parameters from NoteService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> DeleteNote(Request request, ServerCallContext context)
+        {
+            return await _logicService.DeleteNote(request, context);
+        }
+
+        /// <summary>
+        /// Method GetNoteList invokes the method with same name
+        /// with the retrieved parameters from NoteService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> GetNoteList(Request request, ServerCallContext context)
+        {
+            String message = $"About page visited at {DateTime.UtcNow.ToLongTimeString()}";
+            _logger.LogInformation(message);
+            return await _logicService.GetNoteList(request, context);
+        }
+
+        /// <summary>
+        /// Method PostGroup invokes the method with same name
+        /// with the retrieved parameters from GroupService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> PostGroup(PostGroupRequest request, ServerCallContext context)
+        {
+            return await _logicService.PostGroup(request, context);
+        }
+        
+        
+        /// <summary>
+        /// Method DeleteGroup invokes the method with same name
+        /// with the retrieved parameters from GroupService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> DeleteGroup(Request request, ServerCallContext context)
+        {
+            return await _logicService.DeleteGroup(request, context);
+        }
+
+        /// <summary>
+        /// Method RegisterUser invokes the method with same name
+        /// with the retrieved parameters from UserService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<RegisterReply> RegisterUser(RegisterRequest request, ServerCallContext context)
+        {
+            return await _logicService.RegisterUser(request, context);
+        }
+        /// <summary>
+        /// Method ValidateUser invokes the method with same name
+        /// with the retrieved parameters from UserService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> ValidateUser(Request request, ServerCallContext context)
+        {
+            return await _logicService.ValidateUser(request, context);
+        }
+
+        /// <summary>
+        /// Method EditUser invokes the method with same name
+        /// with the retrieved parameters from UserService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> EditUser(EditUserRequest request, ServerCallContext context)
+        {
+            return await _logicService.EditUser(request, context);
+        }
+        /// <summary>
+        /// Method DeleteUser invokes the method with same name
+        /// with the retrieved parameters from UserService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> DeleteUser(UserRequest request, ServerCallContext context)
+        {
+            return await _logicService.DeleteUser(request, context);
+        }
+        /// <summary>
+        /// Method GetUserList invokes the method with same name
+        /// with the retrieved parameters from UserService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> GetUserList(GetUserRequest request, ServerCallContext context)
+        {
+            return await _logicService.GetUserList(request, context);
+        }
+        
+        /// <summary>
+        /// Method PostInvitation invokes the method with same name
+        /// with the retrieved parameters from InvitationService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> PostInvitation(PostInvitationRequest request, ServerCallContext context)
+        {
+            return await _logicService.PostInvitation(request, context);
+        }
+        /// <summary>
+        /// Method GetInvitationList invokes the method with same name
+        /// with the retrieved parameters from InvitationService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> GetInvitationList(Request request, ServerCallContext context)
+        {
+            return await _logicService.GetInvitationList(request, context);
+        }
+
+        /// <summary>
+        /// Method DeleteInvitation invokes the method with same name
+        /// with the retrieved parameters from InvitationService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> DeleteInvitation(Request request, ServerCallContext context)
+        {
+            return await _logicService.DeleteInvitation(request, context);
+        }
+        /// <summary>
+        /// Method GetGroupMemberList invokes the method with same name
+        /// with the retrieved parameters from GroupMembersService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> GetGroupMemberList(Request request, ServerCallContext context)
+        {
+            return await _logicService.GetGroupMemberList(request, context);
+        }
+
+        /// <summary>
+        /// Method GetGroupList invokes the method with same name
+        /// with the retrieved parameters from GroupService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> GetGroupList(Request request, ServerCallContext context)
+        {
+            return await _logicService.GetGroupList(request, context);
+        }
+        /// <summary>
+        /// Method AddGroupMember invokes the method with same name
+        /// with the retrieved parameters from GroupMembersService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> AddGroupMember(AddGroupMemberRequest request, ServerCallContext context)
+        {
+            return await _logicService.AddGroupMember(request, context);
+        }
+
+        /// <summary>
+        /// Method DeleteGroupMember invokes the method with same name
+        /// with the retrieved parameters from GroupMembersService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> DeleteGroupMember(UserRequest request, ServerCallContext context)
+        {
+            return await _logicService.DeleteGroupMember(request, context);
+        }
+
+        /// <summary>
+        /// Method LeaveGroup invokes the method with same name
+        /// with the retrieved parameters from GroupMembersService in 1st tier.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        public override async Task<Reply> LeaveGroup(DeleteGroupMemberRequest request, ServerCallContext context)
+        {
+            return await _logicService.LeaveGroup(request, context);
+        }
+    }
+}
